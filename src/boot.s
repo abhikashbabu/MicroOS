f01da71c6e9d641072286837458acd962df25341
Abhikash Babu <abhikashbabu@gmail.com>
2026-05-05 15:20:19 +0530

    Create boot.s
    This code tells the CPU, Hey, I’m a valid OS, load me.

---
+++ src/boot.s
@@ -0,0 +1,34 @@
+MBALIGN  equ  1 << 0
+MEMINFO  equ  1 << 1
+FLAGS    equ  MBALIGN | MEMINFO
+MAGIC    equ  0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS)
+
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+section .bss
+align 16
+stack_bottom:
+    resb 16384 ; 16 KB stack setup
+stack_top:
+
+section .text
+global _start
+extern kernel_main
+
+_start:
+    ; Stack pointer set karna
+    mov esp, stack_top
+
+    ; Kernel ke main function ko call karna
+    call kernel_main
+
+    ; Agar kernel exit ho jaye, toh CPU ko halt kar do
+    cli
+.hang:
+    hlt
+    jmp .hang
