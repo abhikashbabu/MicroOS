73236f99d8f57bd6db6542afb6e2c47b13db0b8b
Abhikash Babu <abhikashbabu@gmail.com>
2026-05-07 16:57:09 +0530

    Update Files

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,16 @@
 #include "display.h"
+#include "keyboard.h"
 
 void kernel_main() {
     clear_screen();
     
-    // Ab hum kitni bhi lines easily print kar sakte hain!
     print_string("Booting Micro OS...\n");
     print_string("[OK] Kernel Loaded\n");
     print_string("[OK] VGA Text Driver Initialized\n");
-    print_string("\n");
-    print_string("Micro OS - Day 2 Complete! Ready for .ind Apps architecture.\n");
+    print_string("[OK] Keyboard Polling Driver Started\n");
+    print_string("-------------------------------------------\n");
+    print_string("Type something! (Pressing keys will show up below)\n\n");
+    
+    // Keyboard loop start kar do (Ye infinite loop hai)
+    keyboard_read_loop();
 }
