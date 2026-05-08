21438f80e10366507480ba3288c56e91cdc89764
Abhikash Babu <abhikashbabu@gmail.com>
2026-05-09 03:58:20 +0530

    Update string.h
    Update src/string.h (Converting Numbers to Strings)
    The OS cannot directly print numbers like 12 or 45 (it will treat them as ASCII characters). We will need to create a small itoa (Integer to ASCII) function.

--- src/string.h
+++ src/string.h
@@ -1,13 +1,42 @@
 #ifndef STRING_H
 #define STRING_H
 
-// Do strings ko compare karne ka function
+// Strings compare karna (Puraana code)
 static inline int strcmp(const char *s1, const char *s2) {
     while (*s1 && (*s1 == *s2)) {
         s1++;
         s2++;
     }
     return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+}
+
+// Number ko string mein convert karna (NEW)
+static inline void itoa(int num, char* str) {
+    int i = 0;
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return;
+    }
+    
+    // Digits extract karna (ulta order mein aayega)
+    while (num != 0) {
+        int rem = num % 10;
+        str[i++] = rem + '0';
+        num = num / 10;
+    }
+    str[i] = '\0';
+    
+    // String ko seedha karna (Reverse)
+    int start = 0;
+    int end = i - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
 }
 
 #endif
