8f6dc667a3e4f6bf6d3b96c7b55fd0a351555277
Abhikash Babu <abhikashbabu@gmail.com>
2026-05-08 15:47:20 +0530

    updated files
    Update Build Script (build.sh)
    Since we added a new .c file (shell.c), we will need to update the script a little so it can compile and link that file as well.

--- build.sh
+++ build.sh
@@ -5,13 +5,14 @@ echo "Compiling Micro OS..."
 # Compile Assembly
 nasm -f elf32 src/boot.s -o boot.o
 
-# Compile C Files (Kernel & Display)
+# Compile C Files
 gcc -m32 -c src/display.c -o display.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+gcc -m32 -c src/shell.c -o shell.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 gcc -m32 -c src/kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 
-# Link Everything
-ld -m elf_i386 -T linker.ld -o myos.bin boot.o display.o kernel.o
-
+# Link Everything (Notice shell.o added here)
+# Link Everything (Warning hatane ke liye update kiya gaya)
+ld -m elf_i386 -T linker.ld -z noexecstack -o myos.bin boot.o display.o shell.o kernel.o
 echo "Build complete! Running in QEMU..."
 
 # Run Emulator
