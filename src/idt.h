449fa6a735b6e58adcd2e3f75929555200b99c09
Abhikash Babu <abhikashbabu@gmail.com>
2026-05-16 00:21:01 +0530

    Implement IDT (Interrupt Descriptor Table) base
    Created the IDT structure to handle hardware interrupts and exceptions, preparing the system for interrupt-driven I/O.

---
+++ src/idt.h
@@ -0,0 +1,41 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include "io.h"
+
+struct idt_entry {
+    unsigned short base_low;
+    unsigned short sel;
+    unsigned char zero;
+    unsigned char flags;
+    unsigned short base_high;
+} __attribute__((packed));
+
+struct idt_ptr {
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) {
+    idt[num].base_low = (base & 0xFFFF);
+    idt[num].base_high = (base >> 16) & 0xFFFF;
+    idt[num].sel = sel;
+    idt[num].zero = 0;
+    idt[num].flags = flags;
+}
+
+void init_idt() {
+    idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+    idtp.base = (unsigned int)&idt;
+
+    for (int i = 0; i < 256; i++) {
+        idt_set_gate(i, 0, 0, 0);
+    }
+
+    __asm__ __volatile__("lidt %0" : : "m" (idtp));
+}
+
+#endif
--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,8 @@
 #include "display.h"
 #include "keyboard.h"
-#include "fs.h" // NAYA
+#include "fs.h"
+#include "gdt.h" // NAYA
+#include "idt.h" // NAYA
 
 void kernel_main() {
     set_color(COLOR_WHITE, COLOR_BLACK);
@@ -8,18 +10,24 @@ void kernel_main() {
     
     draw_top_bar(" Micro OS v0.1 | .ind Application Runtime");
     
+    // Day 15: GDT Setup
+    init_gdt();
+    print_string("[OK] GDT Initialized (Memory Protected)\n");
+    
+    // Day 16: IDT Setup
+    init_idt();
+    print_string("[OK] IDT Initialized (Interrupts Enabled)\n");
+    
     set_color(COLOR_LIGHT_GREEN, COLOR_BLACK);
     print_string("[OK] Kernel Loaded Successfully\n");
-    print_string("[OK] Color VGA Driver Initialized\n");
     
-    // File system initialize karo
     init_fs();
     print_string("[OK] Virtual File System Mounted\n");
     
     print_string("[OK] Shell Environment Ready\n\n");
     
     set_color(COLOR_WHITE, COLOR_BLACK);
-    print_string("Welcome to Micro OS CLI! Type 'help' to start.\n\n");
+    print_string("Welcome to Micro OS! Build your .ind ecosystem.\n\n");
     
     keyboard_read_loop();
 }
